ab704b68a1b578972c22493bd9db44ea6f69ae86
Chrome Tester <testing.in.chrome@gmail.com>
2016-09-23 00:24:33 -0700

    Test 4

--- src/hello.cc
+++ src/hello.cc
@@ -10,5 +10,6 @@ int main() {
   cout << "This is " << lib::GetName()->c_str() << endl;
   cout << "This is a change to test gerrit" << endl;
   cout << "This is another change to test gerrit" << endl;
+  cout << "Yet another line" << endl;
   return 0;
 }
