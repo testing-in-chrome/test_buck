f3a077309ceb6d9e9867f35271ad057f086e02e3
Chrome Tester <testing.in.chrome@gmail.com>
2016-09-22 23:31:06 -0700

    test gerrit

--- src/hello.cc
+++ src/hello.cc
@@ -8,5 +8,6 @@ int main() {
   cout << "Hello, Buck!" << endl;
   cout << "This is a test!" << endl;
   cout << "This is " << lib::GetName()->c_str() << endl;
+  cout << "This is a change to test gerrit" << endl;
   return 0;
 }
