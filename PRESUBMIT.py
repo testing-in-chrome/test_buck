366453b513305a49bf5e0d89cb6de106c0d04229
Chrome Tester <testing.in.chrome@gmail.com>
2016-09-23 21:30:16 -0700

    Fix more stuff
    BUG=#2
    TEST=manual
    R=development.in.chrome@gmail.com
    
    Review URL: https://codereview.appspot.com/310300043 .

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -8,9 +8,9 @@ def CheckChange(input_api, output_api):
         input_api.canned_checks.CheckLongLines(input_api, output_api, 80))
     results.extend(input_api.canned_checks.CheckOwners(input_api, output_api))
     # Require a BUG= line and a HOW_TO_TEST= line.
-    if not input_api.change.BUG or not input_api.change.HOW_TO_TEST:
+    if not input_api.change.BUG or not input_api.change.TEST:
         results.extend([output_api.PresubmitError(
-            'Must provide a BUG= line and a HOW_TO_TEST line.')])
+            'Must provide a BUG= line and a TEST line.')])
     return results
 
 def CheckChangeOnUpload(input_api, output_api):
