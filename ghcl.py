3d16055a8ee2ecea8f6dee3292130dd84168c1bf
Chrome Tester <testing.in.chrome@gmail.com>
2016-10-20 23:11:37 -0700

    Test ghcl
    Working

--- ghcl.py
+++ ghcl.py
@@ -1,20 +1,25 @@
 from github import Github
 from git import Repo
 import os
+import getpass
 
 if __name__ == "__main__":
   repo = Repo(os.getcwd())
   assert not repo.bare
-  g = Github()
+  username = raw_input("Enter Github username: ")
+  password = getpass.unix_getpass("Password: ")
+  g = Github(username, password)
   urls = []
   for url in repo.remote().urls:
     urls.append(url)
   gh_repo = g.get_repo("testing-in-chrome/test_buck")
   print("Github repo: " + gh_repo.url)
-  print("Active branch: " + str(repo.active_branch))
+  branch = str(repo.active_branch)
+  print("Active branch: " + branch)
 
   print(gh_repo.commits_url)
 
-  pr = gh_repo.create_pull("title1", "body1", "master",
-                           str(repo.active_branch))
-  print pr.url()
+  print(gh_repo.pulls_url)
+  pr = gh_repo.create_pull(title="title1", body="body1", base="master",
+                           head=branch)
+  print pr.url
